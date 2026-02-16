dc9bddacf78bd013534491691f80a75dc04b521e
Kristian <kristianhnielsen@protonmail.com>
2026-02-16 07:32:58 +0100

    refactor objective function to accept separate train and test sets; add mean absolute error logging

--- models/linear_regression.py
+++ models/linear_regression.py
@@ -3,9 +3,9 @@ from experiment_tracking import ExperimentTracker
 from data_handler import DataHandler
 from preprocessing import get_comparative_sales_with_turbine
 from sklearn.linear_model import Ridge
-from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.model_selection import cross_val_score
 import mlflow
 import optuna
 import chime
@@ -13,13 +13,11 @@ import chime
 chime.theme("mario")  # Set a fun theme for notifications
 
 
-def objective(trial: optuna.Trial, X, y):
+def objective(trial: optuna.Trial, X_train, X_test, y_train, y_test):
 
     # Start a new MLflow run for each trial
     with mlflow.start_run(nested=True):
         # Log Optuna trial parameters
-        test_split = 0.1
-        random_state = trial.suggest_int("random_state", 0, 5)
         tolerance = trial.suggest_float("tolerance", 0.00001, 0.1)
         solver = trial.suggest_categorical(
             "solver",
@@ -42,8 +40,6 @@ def objective(trial: optuna.Trial, X, y):
                 "tolerance": tolerance,
                 "solver": solver,
                 "alpha": alpha,
-                "test_split": test_split,
-                "random_state": random_state,
                 "solver_ignores_tol": solver_ignores_tol,
                 "trial_number": trial.number,
                 "positive": positive,
@@ -51,10 +47,6 @@ def objective(trial: optuna.Trial, X, y):
         )
 
         try:
-            X_train, X_test, y_train, y_test = train_test_split(
-                X, y, test_size=test_split, random_state=random_state
-            )
-
             model = Ridge(tol=tolerance, solver=solver, alpha=alpha, positive=positive)  # type: ignore
             model.fit(X_train, y_train)
         except ValueError as e:
@@ -71,10 +63,12 @@ def objective(trial: optuna.Trial, X, y):
         rmse = mean_squared_error(y_test, y_pred) ** 0.5
         r2 = r2_score(y_test, y_pred)
         adjusted_r2 = adjusted_r2_score(y_test, y_pred, X_test.shape[1])
+        mae = mean_absolute_error(y_test, y_pred)
 
         mlflow.log_metrics(
             {
                 "neg_mse_cross_val": neg_mse_cross_val,
+                "mae": mae,
                 "rmse": rmse,
                 "r2": r2,
                 "adjusted_r2": adjusted_r2,
@@ -86,7 +80,7 @@ def objective(trial: optuna.Trial, X, y):
 
         mlflow.log_artifact("models/linear_regression.py", artifact_path="model_code")
 
-        return neg_mse_cross_val
+        return -neg_mse_cross_val
 
 
 def adjusted_r2_score(y_true, y_pred, n_features):
@@ -97,7 +91,7 @@ def adjusted_r2_score(y_true, y_pred, n_features):
 
 
 if __name__ == "__main__":
-    comparison_type = "next"
+    comparison_type = "all"
     exp_tracker = ExperimentTracker(
         algorithm="ridge_regression",
         comparison_type=comparison_type,
@@ -105,14 +99,20 @@ if __name__ == "__main__":
 
     # Load the data
     data_handler = DataHandler()
-    X, y = data_handler.x_y_split(comparison_type=comparison_type)
+    X_train, X_test, y_train, y_test = data_handler.x_y_split(
+        comparison_type=comparison_type
+    )
 
-    study = optuna.create_study(direction="minimize")
+    pruner = optuna.pruners.MedianPruner(n_warmup_steps=5)
+
+    study = optuna.create_study(direction="minimize", pruner=pruner)
     study.optimize(
         lambda trial: objective(
             trial,
-            X,
-            y,
+            X_train,
+            X_test,
+            y_train,
+            y_test,
         ),
         n_trials=1000,
         show_progress_bar=True,
